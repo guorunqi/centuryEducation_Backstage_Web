03e1fcd0bb72d72bccca82016891ec4665ee3fad
guorunqi <827836028@qq.com>
2019-03-20 14:48:12 +0800

    完善

--- config/index.js
+++ config/index.js
@@ -23,13 +23,13 @@ module.exports = {
   },
   dev: {
     env: require('./dev.env'),
-    port: 80,
+    port: 9091,
     autoOpenBrowser: true,
     assetsSubDirectory: 'static',
     assetsPublicPath: '/',
     proxyTable: {
       '/api': {
-        target: 'http://127.0.0.1:8080',
+        target: 'http://47.104.4.114:9090',
         changeOrigin: true,
         pathRewrite: {
           '^/api': ''
